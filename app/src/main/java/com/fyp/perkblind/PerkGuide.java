c7515a9dd0126179da6460fad71228ad2e984fa6
Gulwisha Ahmed <wishakhn@gmail.com>
2021-01-31 19:22:28 +0500

    Perk Guide integration Commit 1 Perk Guide

--- app/src/main/java/com/fyp/perkblind/PerkGuide.java
+++ app/src/main/java/com/fyp/perkblind/PerkGuide.java
@@ -9,12 +9,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PerkGuide extends AppCompatActivity {
     SpeechTextManager speechManager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perk_guide);
+        speechManager = new SpeechTextManager(PerkGuide.this, false);
         initAppBar("PERK GUIDE");
+        startSpeaking();
+    }
+
+    private void startSpeaking() {
+        TextView title, description;
+        title = findViewById(R.id.ptitle);
+        description = findViewById(R.id.pdescription);
+        String str = composeStringToSpeak(title, description);
+        speechManager.setTts_str(str);
+        speechManager.speak(speechManager.getTts_str());
+    }
+
+    private String composeStringToSpeak(TextView title, TextView description) {
+        String str1 = title.getText().toString();
+        String str2 = description.getText().toString();
+        return str1 + str2;
     }
 
     private void initAppBar(String txt) {
@@ -27,5 +43,12 @@ public class PerkGuide extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        speechManager.tts.stop();
+        speechManager.tts.shutdown();
     }
 }
